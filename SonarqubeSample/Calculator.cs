8f7ab43bf708b25384b3fa249ac187c5b55e46de
ashok-sarathi <ashoksarathi004@gmail.com>
2025-01-25 19:52:08 +0530

    Add project files.

---
+++ SonarqubeSample/Calculator.cs
@@ -0,0 +1,25 @@
+﻿namespace SonarqubeSample
+{
+    public class Calculator
+    {
+        public int Addition(int x, int y)
+        {
+            return x + y;
+        }
+
+        public int Subtraction(int x, int y)
+        {
+            return x - y;
+        }
+
+        public int Multiplication(int x, int y)
+        {
+            return x * y;
+        }
+
+        public int Division(int x, int y)
+        {
+            return x / y;
+        }
+    }
+}
